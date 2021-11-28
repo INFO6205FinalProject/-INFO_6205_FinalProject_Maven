f5d56213f27559f25bf91ba359c891ba4614eb40
XingyuHuang23 <huang.xingyu2@northeastern.edu>
2021-11-27 22:37:15 -0500

    maven structure initiate

---
+++ src/main/java/team/TXT.java
@@ -0,0 +1,48 @@
+package team;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+
+public class TXT {
+
+    public static ArrayList<String> read_txt(String add){
+        ArrayList<String> s = new ArrayList<String>();
+        try { // 防止文件建立或读取失败，用catch捕捉错误并打印，也可以throw
+            /* 读入TXT文件 */
+            String pathname = add; // 绝对路径或相对路径都可以，这里是绝对路径，写入文件时演示相对路径
+            File filename = new File(pathname); // 要读取以上路径的input。txt文件
+            InputStreamReader reader = new InputStreamReader(
+                    new FileInputStream(filename)); // 建立一个输入流对象reader
+            BufferedReader br = new BufferedReader(reader); // 建立一个对象，它把文件内容转成计算机能读懂的语言
+            String line = "";
+            line = br.readLine();
+            while (line != null) {
+                line = br.readLine(); // 一次读入一行数据
+                s.add(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return s;
+    }
+    public static void write_txt(String add, ArrayList s){
+        try {
+            File writename = new File(add); // 相对路径，如果没有则要建立一个新的output。txt文件
+            writename.createNewFile(); // 创建新文件
+            BufferedWriter out = new BufferedWriter(new FileWriter(writename));
+            for(Object i : s){
+                //i = (String)(i);
+                out.write(i + "\r\n");
+            }
+            out.flush(); // 把缓存区内容压入文件
+            out.close(); // 最后记得关闭文件
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
