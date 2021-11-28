bda48878790fe6884f58514c8d5046245148fdde
Jim-Qin <1049102910@qq.com>
2021-11-28 11:27:02 -0500

    read txt

--- src/main/java/team/TXT.java
+++ src/main/java/team/TXT.java
@@ -1,30 +1,33 @@
 package team;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.util.List;
 
 public class TXT {
 
-    public static ArrayList<String> read_txt(String add){
+    public static ArrayList<String> read_txt(String addr){
         ArrayList<String> s = new ArrayList<String>();
         try { // 防止文件建立或读取失败，用catch捕捉错误并打印，也可以throw
             /* 读入TXT文件 */
-            String pathname = add; // 绝对路径或相对路径都可以，这里是绝对路径，写入文件时演示相对路径
-            File filename = new File(pathname); // 要读取以上路径的input。txt文件
-            InputStreamReader reader = new InputStreamReader(
-                    new FileInputStream(filename)); // 建立一个输入流对象reader
-            BufferedReader br = new BufferedReader(reader); // 建立一个对象，它把文件内容转成计算机能读懂的语言
-            String line = "";
-            line = br.readLine();
-            while (line != null) {
-                line = br.readLine(); // 一次读入一行数据
-                s.add(line);
+            String filePath = addr;
+            FileInputStream fin = new FileInputStream(filePath);
+            InputStreamReader reader = new InputStreamReader(fin);
+            BufferedReader buffReader = new BufferedReader(reader);
+            String strTmp = "";
+            while((strTmp = buffReader.readLine())!=null){
+                s.add(strTmp);
             }
+            buffReader.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -43,6 +46,14 @@ public class TXT {
             out.close(); // 最后记得关闭文件
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String addr = "/Users/qinhaoyuan/Desktop/INFO 6205/shuffledChinese.txt";
+        ArrayList<String> s = read_txt(addr);
+        for(String i : s){
+            System.out.println(i);
         }
     }
 }
